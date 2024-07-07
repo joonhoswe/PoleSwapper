74a328da231c207f37bc34d9f116b446a9166476
Joonho Jonathan Oh <149014867+joonhoswe@users.noreply.github.com>
2024-07-07 00:25:34 -0400

    updating landing page and config files

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,35 +1,26 @@
 import React from "react";
 import Image from "next/image";
-import Logo from '@assets/logo.png';
-import LinkedIn from '@assets/linkedin.webp';
-import GitHub from '@assets/github.png';
+// import Logo from '@assets/logo.png';
+// import LinkedIn from '@assets/linkedin.webp';
+// import GitHub from '@assets/github.png';
 
 export default function Footer() {
     return (
         <div className="bg-red-500 text-white flex flex-col items-center justify-between w-full h-40 md:h-32 py-4 md:py-2">
             
             <div className="w-28 h-5 md:w-36 md:h-6 flex justify-center items-center mt-2 md:mt-8">
-                <Image src={Logo} alt="Logo" className="w-full h-full" />
+                {/* <Image src={Logo} alt="Logo" className="w-full h-full" /> */}
             </div>
 
             {/* desktop footer */}
             <div className="w-full hidden md:flex flex-col justify-end mt-auto px-4">
                 <div className="flex flex-row items-center justify-between">
-                    <p className="text-xs md:text-sm"> © 2024  </p>
+                    <p className="text-xs md:text-sm"> © 2024 PoleSwapper </p>
 
                     <div className="flex flex-col md:flex-row md:space-x-4 justify-center items-center space-y-2 md:space-y-0">
                         <p className="text-xs md:text-sm"> Terms of Service </p>
-                        <p className="text-xs md:text-sm"> @gmail.com </p>
+                        <p className="text-xs md:text-sm"> poleswapper@gmail.com </p>
                         <p className="text-xs md:text-sm"> Privacy Policy </p>
-                    </div>
-
-                    <div className="flex flex-row space-x-2">
-                        <a className="h-8 w-8 rounded-full bg-white p-1.5 hover:cursor-pointer" href="" target="_blank" rel="noopener noreferrer">
-                            <Image src={LinkedIn} alt="LinkedIn" className="w-full h-full" />
-                        </a>
-                        <a className="h-8 w-8 rounded-full bg-white p-1.5 hover:cursor-pointer" href="" target="_blank" rel="noopener noreferrer">
-                            <Image src={GitHub} alt="GitHub" className="w-full h-full" />
-                        </a>
                     </div>
                 </div>
             </div>
@@ -39,21 +30,12 @@ export default function Footer() {
 
                 <div className="flex flex-col justify-center items-center space-y-1">
                     <p className="text-xs md:text-sm"> Terms of Service </p>
-                    <p className="text-xs md:text-sm"> @gmail.com </p>
+                    <p className="text-xs md:text-sm"> poleswapper@gmail.com </p>
                     <p className="text-xs md:text-sm"> Privacy Policy </p>
                 </div>
 
                 <div className="flex flex-row justify-between items-center mt-2">
-                    <p className="text-xs md:text-sm"> © 2024  </p>
-
-                    <div className="flex flex-row space-x-2">
-                        <a className="h-6 w-6 rounded-full bg-white p-1 hover:cursor-pointer" href="" target="_blank" rel="noopener noreferrer">
-                            <Image src={LinkedIn} alt="LinkedIn" className="w-full h-full" />
-                        </a>
-                        <a className="h-6 w-6 rounded-full bg-white p-1 hover:cursor-pointer" href="" target="_blank" rel="noopener noreferrer">
-                            <Image src={GitHub} alt="GitHub" className="w-full h-full" />
-                        </a>
-                    </div>
+                    <p className="text-xs md:text-sm"> © 2024 PoleSwapper </p>
                 </div>
             </div>
         </div>
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,18 +1,69 @@
 import React, { useState } from 'react';
+import Router from 'next/router';
+import home from '@assets/sam.webp';
 
 export default function Home() {
+  const [school, setSchool] = useState('');
+  const [brand, setBrand] = useState('');
+  const [length, setLength] = useState('');
+  const [weight, setWeight] = useState('');
 
+
+  const router = Router;
+
+
+  const handleFindHomes = () => {
+    if (school.trim() !== '') {
+      router.push(`/schoolmap?school=${school}`);
+    }
+  };
 
   return (
-    <div className="text-white flex flex-col justify-center items-center overflow-hidden relative w-full min-h-screen" 
-      style={{ 
-      // backgroundImage: `url(${background})`,
-      // backgroundSize: 'cover', 
-      // backgroundRepeat: 'no-repeat', 
-      // backgroundPosition: 'center',
-    }}>
+    <div className="flex flex-col items-center w-full min-h-screen text-white font-bold">
+      <div 
+        className='w-full flex flex-grow flex-col space-y-12 md:space-y-4 justify-between items-center py-24' 
+        style={{ 
+          backgroundImage: `url(${home.src})`, 
+          backgroundSize: 'cover',
+          backgroundPosition: 'center'
+        }}
+      >
+        {/* titles */}
+        <div className='text-md sm:text-3xl md:text-4xl flex flex-col space-y-1 sm:space-y-3 md:space-y-6 text-center items-center'>
+          <h1 className='animate-fade-down animate-duration-1000'> The First Online Marketplace for Poles. </h1>
+        </div> 
 
+        {/* school search bar, passes string to Geocoder API for Google Map search */}
+        <div className='flex bg-white h-10 md:h-12 w-3/4 md:w-1/2 rounded-lg md:rounded-2xl animate-fade-down animate-duration-500 animate-delay-1000'>
+          <input 
+            className={`h-full w-5/6 rounded-l-lg md:rounded-l-2xl px-4 text-black outline-none text-xs sm:text-sm lg:text-base`} 
+            placeholder=""
+            value={school}
+            onChange={(e) => setSchool(e.target.value)}
+            onKeyDown={(e) => {
+              if (e.key === 'Enter' || e.key === 'Return') {
+                handleFindHomes();
+              }
+            }}
+          />
+          <button 
+            onClick={handleFindHomes} 
+            className='bg-red-500 h-full w-1/4 flex items-center justify-center rounded-r-lg md:rounded-r-2xl hover:bg-red-600 transition ease-in-out duration-300 text-xs sm:text-sm lg:text-base'
+          >
+            Swap!
+          </button>
+        </div>
+
+      </div>
+
+      <div className='bg-gray-200 w-full items-center md:items-stretch flex flex-col md:flex-row space-y-12 md:space-y-0 md:space-x-6 lg:space-x-12 justify-center px-4 py-10'>
+        {/* {infoCards.map((card, index) => (
+          <div key={index} className={`bg-white text-black h-auto w-64 md:w-80 rounded-2xl flex flex-col items-start space-y-4 p-4 md:p-6 shadow-2xl animate-flip-down animate-duration-1200 animate-delay-${card.delay}`}>
+            <h1 className='text-sm md:text-xl lg:text-2xl'> {card.heading} </h1>
+            <p className='text-sm md:text-sm lg:text-base font-normal'> {card.description} </p>
+          </div>
+        ))} */}
+      </div>
     </div>
   );
 }
-
